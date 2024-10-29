a2c9bd18b3e79921b0d784398bb7589b3bcce736
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-10-29 15:41:38 -0700

    getNumber, clear, and calculate finished
    all basic functions are written and calculator is fully functional now

--- script.js
+++ script.js
@@ -19,21 +19,21 @@ let result = 0;
 
 // one way we can collect our information for operations is to put our numbers into an array
 
-const numArray = [];
-const secondNumArray = [];
+let numArray = [];
+let secondNumArray = [];
 
-const display = document.getElementById('display');
+let display = document.getElementById('display');
 
 
 function getNumber(num) {
     if(step === 0 || step === 1){
         numArray.push(num) // [1,2,5]
         step = 1
-        firstNumber = Number(numArray.join('')) // merges array into one string
+        firstNumber = Number(numArray.join('')) // merges array into one string, wrapping it like this will make sure that you always get a number changing your string into a number
         display.value = firstNumber 
     } else if (step === 2){
         secondNumArray.push(num)
-        secondNumber = Number(secondNumArray.join(''))
+        secondNumber = Number(secondNumArray.join('')) // wrapping with number is the same logic here
         display.value = secondNumber
 
     }
@@ -47,9 +47,28 @@ function getOperator(op) {
 }
 
 function clearDisplay() {
-    console.log('clear display')
+    display.value = 0 
+    firstNumber = null;
+    secondNumber = null; 
+    step = 0;
+    operation = null; 
+    result = 0; 
+    numArray = [];
+    secondNumArray = []; 
 }
 
-const calculateEquals = () =>{
-    console.log('calculate here!')
+const calculateEquals = () => {
+    if(operation === '+') {
+        result = firstNumber + secondNumber
+        display.value = result; 
+    } else if (operation === '-'){
+        result = firstNumber - secondNumber
+        display.value = result; 
+    } else if (operation === '*'){
+        result = firstNumber * secondNumber
+        display.value = result;
+    } else if (operation === '/'){
+        result = firstNumber / secondNumber
+        display.value = result;
+    }
 }
